99ec6800d0cf30afdb1a9131f6ca13436b687fb2
Danliray <danliray@yandex.ru>
2019-11-12 11:14:40 +0300

    Code begin

---
+++ src/test/java/MailruTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.open;
+
+public class MailruTest {
+    @Test
+    public void enterUser() {
+        open("https://www.google.com/");
+
+    }
+}
