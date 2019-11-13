d5a88ce93a5a0a5c92f7d97ee1d0d6e57b5ef7d7
Danliray <danliray@yandex.ru>
2019-11-13 12:00:02 +0300

    Заполнение 1 письма

--- src/test/java/MailruTest.java
+++ src/test/java/MailruTest.java
@@ -1,30 +1,55 @@
+import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 
 import static com.codeborne.selenide.Configuration.browser;
 import static com.codeborne.selenide.Selectors.byText;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selectors.byXpath;
+import static com.codeborne.selenide.Selenide.*;
+
 
 public class MailruTest {
 
+    /*Если нужен firefox
     @BeforeEach
     public void setBrowser() {
-        browser = "chrome";
-    }
+        browser = "firefox";
+    }*/
 
     @Test
     public void enterUser() {
+        //Login
         open("https://mail.ru/");
         $(By.name("login")).setValue("testovyy.akkaunt1@bk.ru").pressEnter();
         $(By.name("password")).setValue("test.akka1").pressEnter();
-        //$("#PH_user-email").shouldHave(text("testovyy.akkaunt1@bk.ru"));
-        //$(By.id("PH_user-email")).shouldHave(text("testovyy.akkaunt1@bk.ru"));
 
+        //Кликаю кнопку "Написать письмо"
         $(byText("Написать письмо")).click();
-        $(By.name("To")).setValue("Тестовое задание Selenide");
+
+        //Получатель
+        $(byXpath("//textarea[@data-original-name=\"To\"]")).setValue("testovyy.akkaunt2@bk.ru");
+
+        //Тема письма
+        $(By.name("Subject")).setValue("Тестовое задание Selenide");
+
+        //Тело письма:
+        SelenideElement mceframe = $(".mceLayout").$(By.tagName("iframe"));
+        SelenideElement mce = $("#tinymce");
+        //Переключаю фрейм
+        switchTo().frame(mceframe);
+        mce.click();
+        mce.clear();
+        mce.sendKeys("Тело письма Selenide");
+        //в дефолтный фрейм
+        switchTo().defaultContent();
 
 
+
+        //SelenideElement PH = $(By.id("PH_user-email"));
+        //SelenideElement PH = $("i#PH_user-email");
+        //$(PH).waitUntil(Condition.exactText(("testovyy.akkaunt1@bk.ru")), 20000);
+        //PH.shouldBe(text("testovyy.akkaunt1@bk.ru"));
+        //$(byXpath("//a[@data-name=\"compose\"]")).click();
     }
 }
