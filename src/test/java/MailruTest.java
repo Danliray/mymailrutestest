979f18ebdac52bafff7da060f846591b1fbe8299
Andy <danliray@yandex.ru>
2019-11-15 00:01:51 +0300

    Проблема авторизации во второй аккаунт

--- src/test/java/MailruTest.java
+++ src/test/java/MailruTest.java
@@ -4,12 +4,15 @@ import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
 
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Configuration.baseUrl;
 import static com.codeborne.selenide.Configuration.browser;
 import static com.codeborne.selenide.Selectors.*;
 import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
 
 
 public class MailruTest {
@@ -22,29 +25,58 @@ public class MailruTest {
 
     @Test
     public void enterUser() {
-        int numMails = 6;
+
+        String loginUser1 = "testovyy.akkaunt1@bk.ru";
+        String passUser1 = "test.akka1";
+        String loginUser2 = "testovyy.akkaunt2@bk.ru";
+        String passUser2 = "test.akka2";
+        int numMails = 1;
         String email = "testovyy.akkaunt2@bk.ru";
-        String subj = "Тестовое задание Selenide";,
-        String bodyMail = "Тело письма Selenide";,
+        String subj = "Тестовое задание Selenide";
+        String bodyMail = "Тело письма Selenide";
         String myUrlFile = "C:\\selenide\\mdm.txt";
         Configuration.holdBrowserOpen = true;
-        //Login
-        open("https://mail.ru/");
-        $(By.name("login")).setValue("testovyy.akkaunt1@bk.ru").pressEnter();
-        $(By.name("password")).setValue("test.akka1").pressEnter();
-
-        loginUser();
-        //Проверка авторизации
-        $$("i.x-ph__menu__button__text_auth").findBy(text("testovyy.akkaunt2@bk.ru"));
-
         //Configuration.timeout = 10000;
+        //открываю первую вкладку
+        open("https://mail.ru/");
+
+        //авторизация под первым аккаунтом и проверяю авторизацию
+        autorizationUser(loginUser1, passUser1);
+        //отправляю письма на второй аккаунт и проверяю отправку писем
         sendMail(numMails, email, subj, bodyMail, myUrlFile);
+        //переключаюсь на 2 вкладку
+        //JavascriptExecutor jse = (JavascriptExecutor)getWebDriver();
+        //jse.executeScript("window.open('https://mail.ru/');");
+        //switchTo().window(1);
+        $("span#PH_authMenu_button").click();
+        $("a#PH_loginAnotherLink").click();
+        //авторизация под вторым аккаунтом и проверяю авторизацию
+
+
+       // $("."));
+        //switchTo().activeElement().sendKeys(loginUser2);
+        //switchTo().frame($("frame.ag-popup__frame__layout__iframe"));
+        //System.out.println(switchTo().frame($(".ag-popup__frame__layout__iframe")).getPageSource());
+        //witchTo().frame($(".ag-popup__frame__layout__iframe")).getPageSource();
+        switchTo().frame($(".ag-popup__frame__layout__iframe"));
+        //switchTo().innerFrame($(ByTagName("frame")));
+        //System.out.println($$(By.tagName(frame);
+        switchTo().activeElement().sendKeys(loginUser2);
+        //autorizationUser(loginUser2, passUser2);
+        //sendMail(numMails, email, subj, bodyMail, myUrlFile);
+        //$("#root").$(By.name("login")).setValue(loginUser2).pressEnter();
+        //$("#root").$(By.name("password")).setValue(passUser2).pressEnter();
 
 
     }
 
-    public void loginUser() {
+    public void autorizationUser(String loginUser, String passUser) {
+        //Ввод логина и пароля
+        $(By.name("login")).shouldBe(enabled).setValue(loginUser).pressEnter();
+        $(By.name("password")).shouldBe(enabled).setValue(passUser).pressEnter();
 
+        //Проверка авторизации
+        $$("i.x-ph__menu__button__text_auth").findBy(text("testovyy.akkaunt2@bk.ru"));
     }
 
     public void sendMail(int numMails, String email, String subj, String bodyMail, String myUrlFile) {
